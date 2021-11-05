010bb9fce95a4e2f862eefcf2c44aed46cda9c1f
Trung Do <trung.do@episerver.com>
2021-11-05 17:06:19 +0700

    style

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ let form = document.getElementById("form-qr");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     let accountName = "Vũ Đình Toàn";
-    // accountName = "Vu Dinh Toan";
     let content = document.getElementById("input-transfer-content").value;
     let amount = document.getElementById("input-transfer-amount").value;
     let bankName = "Vietcombank";
     let bankNumber = "1111514131";
     let img = document.getElementById("qr-image");
-    let imageUrl = `https://api.vietqr.io/${bankName}/${bankNumber}/${amount}/${content}/vietqr_net_2.jpg?accountName=${encodeURI(accountName)}`;
+    let imageUrl = `https://api.vietqr.io/${bankName}/${bankNumber}/${amount}/${encodeURI(content)}/vietqr_net_2.jpg?accountName=${encodeURI(accountName)}`;
     img.src = imageUrl;
 });
